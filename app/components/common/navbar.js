0b7cad0af36bdd54b98d0f29dfe6e8e914f764e6
AMcdougal <31479662+AdamMcDougal@users.noreply.github.com>
2024-07-18 17:31:02 -0600

    Fix navbar
    Fixed the navbar from not updating css due to usestate issues.

--- app/components/common/navbar.js
+++ app/components/common/navbar.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Navbar({ title }) {
   return (
     <nav className="header">
-      <a href="./homepage">
+      <Link href="./homepage">
         <Image src="/logov2.png" alt="logo" width={200} height={30} />
-      </a>
+      </Link>
       <h1 className="nav-title">{title}</h1>
-      <a href="../../">
-        <Image src="/logout.ico" alt="logout" width={50} height={30} />
-      </a>
+      <Link href="../../">
+          <Image src="/logout.ico" alt="logout" width={50} height={30} />
+      </Link>
     </nav>
   );
 }
