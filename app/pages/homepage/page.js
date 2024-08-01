ef885f3ee38f85804749eb4d2c6e30ac2aba01ea
AMcdougal <31479662+AdamMcDougal@users.noreply.github.com>
2024-08-01 17:51:44 -0600

    Add basic database functionality
    Added basic database functionality
    
    Made it so that you can only view the new projects and homepage if you are signed in with GitHub. but you can still view canvas either way.
    
    Cleaned up some code in the hompage\page.js so it wasn't as messy
    
    Co-Authored-By: R.Smith <126202532+Rhiannacsmith@users.noreply.github.com>

--- app/pages/homepage/page.js
+++ app/pages/homepage/page.js
@@ -7,51 +7,16 @@ export default function Homepage() {
       <div className="main-cont">
         <div className="home-sidebar">
           <ul>
-            <li>
-              <a className="new-button" href="./new-project">
-                + New Project
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Recent Projects
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Collaborate
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Import
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Export
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Library
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Tutorials
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a className="button" href="./new-project">
-                Help
-              </a>
-            </li>
+            <li><a className="new-button" href="./new-project">+ New Project</a></li>
+            <li><a className="button" href="./new-project">Load Project</a></li>
+            <li><a className="button" href="./new-project">Recent Projects</a></li>
+            <li><a className="button" href="./new-project">Collaborate</a></li>
+            <li><a className="button" href="./new-project">Import</a></li>
+            <li><a className="button" href="./new-project">Export</a></li>
+            <li><a className="button" href="./new-project">Library</a></li>
+            <li><a className="button" href="./new-project">Tutorials</a></li>
+            <li><a className="button" href="./new-project">Settings</a></li>
+            <li><a className="button" href="./new-project">Help</a></li>
           </ul>
         </div>
       </div>
